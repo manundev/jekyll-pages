82e322576c4c30142d62af7e1b9de0aa472a021a
manunio <max.manu.nair@gmail.com>
2021-05-21 20:03:11 +0530

    Revert "update"
    This reverts commit 7847631bf0b9e65940bb9b4afdbb9a1561760b65.

--- docs/plugins/gems/jekyll-opal-0.3.0/lib/jekyll-opal.rb
+++
@@ -1,9 +0,0 @@
-require "jekyll-opal/version"
-require "jekyll/converters/opal"
-require "jekyll/generators/opal"
-
-module Jekyll
-  module Opal
-    OPAL_LIB_LOCATION = File.join("js", "opal.js").freeze
-  end
-end
--- docs/plugins/gems/jekyll-opal-0.3.0/lib/jekyll-opal/version.rb
+++
@@ -1,5 +0,0 @@
-module Jekyll
-  module Opal
-    VERSION = "0.3.0"
-  end
-end
